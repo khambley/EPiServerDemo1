d09e98e7ae744565eabb3024275c3ff458939d9b
Katherine Hambley <superdesigngirl@mac.com>
2017-07-26 08:03:46 -0500

    Initial commit

---
+++ EpiServerDemo1/Models/Blocks/TopNavBlock.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using EPiServer.Core;
+using EPiServer.DataAbstraction;
+using EPiServer.DataAnnotations;
+
+namespace EpiServerDemo1.Models.Blocks
+{
+    [ContentType(DisplayName = "TopNavBlock", GUID = "d43897c9-9a2e-4011-a9f4-67036c1af1fd", Description = "")]
+    public class TopNavBlock : BlockData
+    {
+
+        [CultureSpecific]
+        [Display(
+            Name = "Name",
+            Description = "Name field's description",
+            GroupName = SystemTabNames.Content,
+            Order = 1)]
+        public virtual string Name { get; set; }
+
+    }
+}
---
+++ EpiServerDemo1/Models/Pages/StandardPage.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using EPiServer.Core;
+using EPiServer.DataAbstraction;
+using EPiServer.DataAnnotations;
+using EPiServer.SpecializedProperties;
+
+namespace EpiServerDemo1.Models.Pages
+{
+    [ContentType(DisplayName = "StandardPage", GUID = "dcc26531-cbf9-4cca-bfca-f6040b294796", Description = "")]
+    public class StandardPage : PageData
+    {
+
+        [CultureSpecific]
+        [Display(
+            Name = "Main body",
+            Description = "The main body will be shown in the main content area of the page, using the XHTML-editor you can insert for example text, images and tables.",
+            GroupName = SystemTabNames.Content,
+            Order = 1)]
+        public virtual XhtmlString MainBody { get; set; }
+
+    }
+}
